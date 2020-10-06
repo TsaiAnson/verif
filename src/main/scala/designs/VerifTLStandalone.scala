f17df70dbacc5da4dc964fbe33c3a99053960bcd
Anson Tsai <yuancheng.tsai@berkeley.edu>
2020-10-06 15:23:15 -0700

    Working example of basic poke/peeks with TL standalone

--- src/main/scala/designs/VerifTLStandalone.scala
+++ src/main/scala/designs/VerifTLStandalone.scala
@@ -29,27 +29,17 @@ trait VerifTLStandaloneBlock extends LazyModule {
 //  }}
 
   val ioInNode = BundleBridgeSource(() => TLBundle(standaloneParams))
-//  val ioOutNode = BundleBridgeSink[TLBundle]()
   val TLNode: TLNode
-  val client: TLNode
 
-  TLNode := client
+  TLNode :=
+    BundleBridgeToTL(TLClientPortParameters(Seq(TLClientParameters("bundleBridgeToTL")))) :=
+    ioInNode
 
-//  ioOutNode :=
-//    TLToBundleBridge(TLManagerPortParameters(Seq(TLManagerParameters(address = Seq(AddressSet(0x2000, 0xfff)))), beatBytes = 8)) :=
-//    TLNode :=
-//    BundleBridgeToTL(TLClientPortParameters(Seq(TLClientParameters("bundleBridgeToTL")))) :=
-//    ioInNode
-//
-//  val in = InModuleBody { ioInNode.makeIO() }
-//  val out = InModuleBody { ioOutNode.makeIO() }
+  val in = InModuleBody { ioInNode.makeIO() }
 }
 
 class VerifTLPassthrough(implicit p: Parameters) extends LazyModule  {
   val device = new SimpleDevice("veriftlpassthrough", Seq("veriftldriver,veriftlmonitor,testclient")) // Not sure about compatibility list
-//  val TLNode = TLIdentityNode()
-//
-//  TLNode := node
 
   val TLNode = TLRegisterNode(
     address = Seq(AddressSet(0x0, 0xfff)),
@@ -57,32 +47,82 @@ class VerifTLPassthrough(implicit p: Parameters) extends LazyModule  {
     beatBytes = 8,
     concurrency = 1)
 
-  // Adding client node just for testing --- Temporary
-  val client = TLClientNode(Seq(TLClientPortParameters(Seq(TLClientParameters(
-    name = "testclient",
-    sourceId = IdRange(0,1),
-    requestFifo = true,
-    visibility = Seq(AddressSet(0x1000, 0xfff)))))))
+//  // Adding client node just for testing --- Temporary
+//  val client = TLClientNode(Seq(TLClientPortParameters(Seq(TLClientParameters(
+//    name = "testclient",
+//    sourceId = IdRange(0,1),
+//    requestFifo = true,
+//    visibility = Seq(AddressSet(0x1000, 0xfff)))))))
 
   lazy val module = new LazyModuleImp(this) {
-    val bigReg = RegInit(10.U(64.W))
-    val mediumReg = RegInit(11.U(32.W))
-    val smallReg = RegInit(12.U(16.W))
-
-    val tinyReg0 = RegInit(13.U(4.W))
-    val tinyReg1 = RegInit(14.U(4.W))
+    val bigReg1 = RegInit(10.U(64.W))
+    val bigReg2 = RegInit(11.U(64.W))
+    val bigReg3 = RegInit(12.U(64.W))
+    val bigReg4 = RegInit(13.U(64.W))
 
     // Will try to implement hardware FIFO (using Queue) for later examples
     TLNode.regmap(
-      0x00 -> Seq(RegField(64, bigReg)),
-      0x08 -> Seq(RegField(32, mediumReg)),
-      0x0C -> Seq(RegField(16, smallReg)),
-      0x0E -> Seq(RegField(4, tinyReg0),
-                  RegField(4, tinyReg1))
+      0x00 -> Seq(RegField(64, bigReg1)),
+      0x08 -> Seq(RegField(64, bigReg2)),
+      0x10 -> Seq(RegField(64, bigReg3)),
+      0x18 -> Seq(RegField(64, bigReg4))
     )
-
-    val (in, inE) = TLNode.in(0)
-    val (cli, cliE) = client.out(0)
   }
 }
+
+//class RegMaster(implicit p: Parameters) extends LazyModule {
+//  val device = new SimpleDevice("veriftlpassthrough", Seq("veriftldriver,veriftlmonitor,testclient"))
+//
+//  val RegNode = TLRegisterNode(
+//    address = Seq(AddressSet(0x0, 0xfff)),
+//    device = device,
+//    beatBytes = 8,
+//    concurrency = 1)
+//
+//  lazy val module = new LazyModuleImp(this) {
+//    val bigReg = RegInit(10.U(64.W))
+//    val mediumReg = RegInit(11.U(32.W))
+//    val smallReg = RegInit(12.U(16.W))
+//
+//    val tinyReg0 = RegInit(13.U(4.W))
+//    val tinyReg1 = RegInit(14.U(4.W))
+//
+//    // Will try to implement hardware FIFO (using Queue) for later examples
+//    RegNode.regmap(
+//      0x00 -> Seq(RegField(64, bigReg)),
+//      0x08 -> Seq(RegField(32, mediumReg)),
+//      0x0C -> Seq(RegField(16, smallReg)),
+//      0x0E -> Seq(RegField(4, tinyReg0),
+//        RegField(4, tinyReg1))
+//    )
+//
+//    val (reg, regE) = RegNode.in(0)
+//  }
+//}
+//
+//class Client1(implicit p: Parameters) extends LazyModule {
+//  val client = TLClientNode(Seq(TLClientPortParameters(Seq(TLClientParameters(
+//    name = "testclient",
+//    sourceId = IdRange(0,1),
+//    requestFifo = true,
+//    visibility = Seq(AddressSet(0x1000, 0xfff)))))))
+//
+//  lazy val module = new LazyModuleImp(this) {
+//    val (cli, cliE) = client.out(0)
+//  }
+//}
+//
+//class MasterClient(implicit p: Parameters) extends LazyModule {
+//  val master = LazyModule(new RegMaster)
+//  val client = LazyModule(new Client1)
+//
+//  master.RegNode := client.client
+//
+//  lazy val module = new LazyModuleImp(this) {
+//    val reg = master.module.reg
+//    val regE = master.module.regE
+//    val cli = client.module.cli
+//    val cliE = client.module.cliE
+//  }
+//}
 
--- src/test/scala/DSPToolsTest.scala
+++ src/test/scala/DSPToolsTest.scala
@@ -13,42 +13,123 @@ import freechips.rocketchip.diplomacy.LazyModule
 import freechips.rocketchip.system.BaseConfig
 import freechips.rocketchip.tilelink._
 import chisel3.experimental.BundleLiterals._
+import freechips.rocketchip.subsystem.WithoutTLMonitors
+import freechips.rocketchip.util._
+
+case object MyBundleData extends DataKey[UInt]("data")
+case class MyBundleDataField(width: Int) extends SimpleBundleField(MyBundleData)(Output(UInt(width.W)), 0.U)
 
 class DSPToolsTest extends FlatSpec with ChiselScalatestTester {
-  implicit val p: Parameters = Parameters.empty.asInstanceOf[Parameters]
+//  implicit val p: Parameters = Parameters.empty.asInstanceOf[Parameters]
   //  implicit val p: Parameters = (new BaseConfig).toInstance
+  implicit val p: Parameters = new WithoutTLMonitors
 
   def standaloneParams = TLBundleParameters(addressBits = 64, dataBits = 64, sourceBits = 1,
     sinkBits = 1, sizeBits = 6,
     echoFields = Seq(), requestFields = Seq(), responseFields = Seq(),
     hasBCE = false)
 
+  val testBundleMap = new BundleMap(Seq(MyBundleDataField(1)))
+
   it should "DSPTools Test" in {
     val TLPassthrough = LazyModule(new VerifTLPassthrough with VerifTLStandaloneBlock)
     test(TLPassthrough.module).withAnnotations(Seq(TreadleBackendAnnotation, WriteVcdAnnotation)) { c =>
 
-      println("Got here")
+      val a = TLPassthrough.in.a
 
-//      val protoTLBundleA = new TLBundleA(standaloneParams)
-//      val testA = protoTLBundleA.Lit(_.opcode -> 0.U(3.W), _.param -> 0.U(3.W), _.size -> 2.U(6.W), _.source -> 1.U(1.W),
-//        _.address -> 0.U(8.W))
-//      val a = c.in.head.a
-//      // Poke fails as testA is a hardware and not a chisel type?
-//      a.pokePartial(Decoupled(testA))
+      a.bits.opcode.poke(0.U)
+      a.bits.param.poke(0.U)
+      a.bits.address.poke(0x10.U)
+      a.bits.size.poke(2.U)
+      a.bits.source.poke(1.U)
+      a.bits.mask.poke(0xfff.U)
+      a.bits.data.poke(0.U)
+
+      a.valid.poke(true.B)
+
+      while (a.ready.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+      c.clock.step(1)
+      a.valid.poke(false.B)
+
+      // Checking for response
+      val d = TLPassthrough.in.d
+      d.ready.poke(true.B)
+      while (!d.valid.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+      // Read output
+      println(s"opcode ${d.bits.opcode.peek()}")
+      println(s"param ${d.bits.param.peek()}")
+      println(s"size ${d.bits.size.peek()}")
+      println(s"source ${d.bits.source.peek()}")
+      println(s"sink ${d.bits.sink.peek()}")
+      println(s"data ${d.bits.data.peek()}")
+      println(s"corrupt ${d.bits.corrupt.peek()}")
+
+      c.clock.step(1)
+      d.ready.poke(false.B)
+      c.clock.step(5)
+
+      a.bits.opcode.poke(4.U)
+      a.bits.param.poke(0.U)
+      a.bits.address.poke(0x10.U)
+      a.bits.size.poke(2.U)
+      a.bits.source.poke(1.U)
+      a.bits.mask.poke(0xfff.U)
+      a.bits.data.poke(0.U)
+
+      a.valid.poke(true.B)
+
+      while (a.ready.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+      c.clock.step(1)
+      a.valid.poke(false.B)
+
+      // Checking for response
+      d.ready.poke(true.B)
+      while (!d.valid.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+      // Read output
+      println(s"opcode ${d.bits.opcode.peek()}")
+      println(s"param ${d.bits.param.peek()}")
+      println(s"size ${d.bits.size.peek()}")
+      println(s"source ${d.bits.source.peek()}")
+      println(s"sink ${d.bits.sink.peek()}")
+      println(s"data ${d.bits.data.peek()}")
+      println(s"corrupt ${d.bits.corrupt.peek()}")
+
+      c.clock.step(1)
+      d.ready.poke(false.B)
+      c.clock.step(5)
 
       // Trying to use helper function with edge
-//      val inE = c.inE.head
+//      val inE = c.inE
 //      val (valid, testA) = inE.Get(1.U, 0.U, 6.U)
 //      println(valid)
-      val clientE = c.cliE
-      val (valid, testA) = clientE.Get(1.U, 0.U, 6.U)
-      println(valid)
+//      val clientE = c.cliE
+//      val (valid, testA) = clientE.Get(1.U, 0.U, 6.U)
+//      println(valid)
 
 //      // Peeking some value as test
 //      println("Got here 2")
 //
 //      val d = c.out.head.d
 //      println(d.bits.data)
+
+
+      // // Poking with bundle literals is not working, leaving here for reference
+      // val protoTLBundleA = new TLBundleA(standaloneParams)
+      // println(TLPassthrough.in.params)
+      // val testA = protoTLBundleA.Lit(_.opcode -> 0.U(3.W), _.param -> 0.U(3.W), _.size -> 2.U(6.W), _.source -> 1.U(1.W),
+      //   _.address -> 0.U(8.W), _.user -> testBundleMap, _.echo -> testBundleMap,
+      //   _.address -> 0.U(8.W),
+      //   _.mask -> 0xfff.U, _.data -> 0.U, _.corrupt -> false.B)
+      // // Poke fails as testA is a hardware and not a chisel type?
+      // a.pokePartial(Decoupled(testA))
 
       assert(true)
     }
