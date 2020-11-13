88c580e01ede6fb57f67d76056a1b8598b7e8eb9
Anson Tsai <yuancheng.tsai@berkeley.edu>
2020-11-12 17:13:56 -0800

    Added basic test for Transaction equality mixin

---
+++ src/test/scala/TLVIPTest.scala
@@ -0,0 +1,88 @@
+package verif
+
+import org.scalatest._
+import chisel3._
+import chiseltest._
+import scala.util.Random
+
+class TLVIPTest extends FlatSpec with ChiselScalatestTester {
+  it should "Test Transaction MIXIN Equality" in {
+    val randGen = new Random()
+    var randomUInt = 0.U
+    var randomAddrUInt = 0.U
+
+    // Checking GET
+    for (_ <- 0 to 500) {
+      randomAddrUInt = randGen.nextInt(Int.MaxValue).U
+      randomUInt = randGen.nextInt(Int.MaxValue).U
+      // Checking identity
+      val g = Get(addr = randomAddrUInt)
+      assert(g == g)
+
+      // Checking equality
+      assert(g == Get(addr = randomAddrUInt))
+
+      // Checking non-equality
+      assert(g != Get(addr = randGen.nextInt(Int.MaxValue).U))
+      assert(g != FullPut(addr = randomAddrUInt, data = randomUInt))
+      assert(g != AccessAck())
+      assert(g != AccessAckData(data = randomUInt))
+    }
+
+    // Checking FullPut
+    for (_ <- 0 to 500) {
+      randomAddrUInt = randGen.nextInt(Int.MaxValue).U
+      randomUInt = randGen.nextInt(Int.MaxValue).U
+      // Checking identity
+      val fp = FullPut(addr = randomAddrUInt, data = randomUInt)
+      assert(fp == fp)
+
+      // Checking equality
+      assert(fp == FullPut(addr = randomAddrUInt, data = randomUInt))
+
+      // Checking non-equality
+      assert(fp != FullPut(addr = randGen.nextInt(Int.MaxValue).U, data = randGen.nextInt(Int.MaxValue).U))
+      assert(fp != Get(addr = randomAddrUInt))
+      assert(fp != AccessAck)
+      assert(fp != AccessAckData(data = randomUInt))
+    }
+
+    // Checking AccessAck
+    for (_ <- 0 to 10) {
+      randomAddrUInt = randGen.nextInt(Int.MaxValue).U
+      randomUInt = randGen.nextInt(Int.MaxValue).U
+      val ack = AccessAck()
+
+      // Checking identity
+      assert(ack == ack)
+
+      // Checking equality
+      assert(ack == AccessAck())
+
+      // Checking non-equality
+      assert(ack != Get(addr = randomAddrUInt))
+      assert(ack != FullPut(addr = randomAddrUInt, data = randomUInt))
+      assert(ack != AccessAckData(data = randomUInt))
+      assert(ack != AccessAckData(data = 0.U))
+    }
+
+    // Checking AccessAckData
+    for (_ <- 0 to 500) {
+      randomAddrUInt = randGen.nextInt(Int.MaxValue).U
+      randomUInt = randGen.nextInt(Int.MaxValue).U
+      // Checking identity
+      val ackd = AccessAckData(data = randomUInt)
+      assert(ackd == ackd)
+
+      // Checking equality
+      assert(ackd == AccessAckData(data = randomUInt))
+
+      // Checking non-equality
+      assert(ackd != AccessAckData(data = randGen.nextInt(Int.MaxValue).U))
+      assert(ackd != Get(addr = randomUInt))
+      assert(ackd != FullPut(addr = randomAddrUInt, data = randomUInt))
+      assert(ackd != AccessAck)
+
+    }
+  }
+}
