c667cc0d0866ec8c04805d4f74b537afe8733345
TsaiAnson <ansonytsai@gmail.com>
2021-02-01 14:03:22 -0800

    Added future properties

--- tilelink/src/TLProperty.scala
+++ tilelink/src/TLProperty.scala
@@ -26,19 +26,33 @@ Notes: Properties should only return false if the condition is true and property
 
 case class TLSelfPropertyA(property: TLBundleA => Boolean) extends TLSelfProperty[TLBundleA](property)
 case class TLSelfPropertyD(property: TLBundleD => Boolean) extends TLSelfProperty[TLBundleD](property)
-// Can continue for other TLChannel types BCE
+// Can continue for other TLChannel types BCE, and generic type (TLChannel)
 
-//// Future cycles (lax), nonconsecutive
-//// Won't need a strict (consecutive) one for TL, but will need for general case
-//// TODO Handle repetitions and/or overlapping
-//class TLFutureProperty[T](init_property: T => Boolean, future_property: T => Boolean) {
-//  def check(input: T): Boolean = {
-//    property(input)
-//  }
-//
-//  def check(input: Seq[T], count: Int): Boolean = {
-//
-//  }
-//}
+// Future cycles (lax), nonconsecutive
+// Won't need a strict (consecutive) one for TL, but will need for general case
+// TODO Handle repetitions? Overlapping? Init properties that are same as future property (e.g. burst)
+// TODO Add way to have dependent properties (e.g. if size is burst, future property checks if constant fields match)
+class TLFutureProperty[T](init_property: T => Boolean, future_property: T => Boolean, count: Int) {
+  var remaining_count = 0
+  def checkInit(input: T): Unit = {
+    if (init_property(input)) remaining_count += count
+  }
 
-// Thoughts: Can return dangling bundles?
+  def checkFuture(input: T): Unit = {
+    if (future_property(input)) remaining_count -= 1
+  }
+
+  // Cannot handle overlapping/concurrent properties (e.g. Get(source = 0) -> Get(source = 1) -> AccessAckData(1) -> AccessAckData(0))
+  // Note: Currently assumes complete transaction sequence
+  def check(input: Seq[T]): Boolean = {
+    for (i <- input) {if (remaining_count == 0) checkInit(i) else checkFuture(i)}
+    remaining_count == 0
+  }
+}
+
+case class TLFuturePropertyA(property1: TLBundleA => Boolean, property2: TLBundleA => Boolean, count: Int = 1) extends
+  TLFutureProperty[TLBundleA](property1, property2, count)
+case class TLFuturePropertyD(property1: TLBundleD => Boolean, property2: TLBundleD => Boolean, count: Int = 1) extends
+  TLFutureProperty[TLBundleD](property1, property2, count)
+case class TLFuturePropertyAD(property1: TLChannel => Boolean, property2: TLChannel => Boolean, count: Int = 1) extends
+  TLFutureProperty[TLChannel](property1, property2, count)
--- tilelink/test/TLPropertyTest.scala
+++ tilelink/test/TLPropertyTest.scala
@@ -6,7 +6,7 @@ import chisel3.experimental.BundleLiterals._
 import TLTransaction._
 import chipsalliance.rocketchip.config.Parameters
 import freechips.rocketchip.subsystem.WithoutTLMonitors
-import freechips.rocketchip.tilelink.{TLBundleA, TLBundleParameters}
+import freechips.rocketchip.tilelink.{TLBundleA, TLBundleD, TLBundleParameters, TLChannel}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class TLPropertyTest extends AnyFlatSpec with ChiselScalatestTester {
@@ -61,5 +61,79 @@ class TLPropertyTest extends AnyFlatSpec with ChiselScalatestTester {
       Get(0x18)
     )
     assert(!getProperty.check(inputTransactionsBad).foldLeft(true)(_ & _))
+  }
+
+  it should "test future property in bursts" in {
+    def twoBeatBurst(t: TLBundleA): Boolean = {
+      t.size.litValue() == 4
+    }
+    val twoBeatBurstProperty = TLFuturePropertyA(twoBeatBurst, twoBeatBurst, 1)
+
+    val input = PutBurst(0x0, Seq(0x1234, 0x5678), 0) ++
+      Seq(Get(0x0),
+        Get(0x08),
+        Put(0x0, 0)) ++
+      PutBurst(0x0, Seq(0x1234, 0x5678), 0) ++
+      PutBurst(0x0, Seq(0x1234, 0x5678), 0) ++
+      Seq(Get(0x0),
+        Get(0x08),
+        Put(0x0, 0)) ++
+      PutBurst(0x0, Seq(0x1234, 0x5678), 0)
+    assert(twoBeatBurstProperty.check(input))
+
+    val inputBad = PutBurst(0x0, Seq(0x1234, 0x5678), 0) ++
+      Seq(Get(0x0),
+        Get(0x08),
+        Put(0x0, 0)) ++
+      PutBurst(0x0, Seq(0x1234, 0x5678), 0) ++
+      Seq(new TLBundleA(params).Lit(_.opcode -> TLOpcodes.PutFullData.U, _.param -> 0.U, _.size -> 4.U,
+        _.source -> 0.U, _.address -> 0x0.U, _.mask -> 0xff.U, _.corrupt -> 0.B, _.data -> 0.U)) ++
+      Seq(Get(0x0),
+        Get(0x08),
+        Put(0x0, 0)) ++
+      PutBurst(0x0, Seq(0x1234, 0x5678), 0)
+    assert(!twoBeatBurstProperty.check(inputBad))
+  }
+
+  it should "test future property in Get -> AccessAckData transaction (single beat, no concurrent txns)" in {
+    def getReq(t: TLChannel): Boolean = {
+      t match {
+        case t: TLBundleA => t.opcode.litValue() == TLOpcodes.Get
+        case _ => false
+      }
+    }
+    def accessAckDataResp(t: TLChannel): Boolean = {
+      t match {
+        case t: TLBundleD => t.opcode.litValue() == TLOpcodes.AccessAckData
+        case _ => false
+      }
+    }
+    val getTxnProperty = TLFuturePropertyAD(getReq, accessAckDataResp)
+
+    val input = Seq(
+      Get(0x0),
+      AccessAckData(0x1234, 0),
+      Get(0x1),
+      AccessAckData(0x5678, 0)
+    )
+    assert(getTxnProperty.check(input))
+
+    val inputBadOne = Seq(
+      Get(0x0),
+      AccessAckData(0x1234, 0),
+      Get(0x1)
+    )
+    assert(!getTxnProperty.check(inputBadOne))
+
+    val inputBadTwo = Seq(
+      Get(0x0),
+      AccessAckData(0x1234, 0),
+      Get(0x1),
+      Get(0x2),
+      AccessAckData(0x1234, 0)
+    )
+    // TODO This one "passes" the check since the third Get gets skipped over
+    // Ways to get around this: Add checks for matching source ID (as sourceID is unique across unique transactions)
+    assert(!getTxnProperty.check(inputBadTwo))
   }
 }
