dd1ce2b86a5736bd20ac2cdf29bf4fcf7335feea
TsaiAnson <ansonytsai@gmail.com>
2021-03-10 12:26:50 -0800

    SLSequence now allows implications and timeoperators to be first. Moved no implication warning to SLProperty

--- core/src/speclang/src/SLSequence.scala
+++ core/src/speclang/src/SLSequence.scala
@@ -18,7 +18,7 @@ class Sequence[T,H,M](input: SequenceElement*) {
           condensed.update(condensed.size-1, condensed.last.asInstanceOf[AtmProp[T,H,M]] & a)
         else condensed += a
       case t: TimeOp =>
-        if (input(i-1).isInstanceOf[TimeOp]) println(s"WARNING: Detected 2 TimeOps in a row. Ignoring second one: $t")
+        if (i != 0 && input(i-1).isInstanceOf[TimeOp]) println(s"WARNING: Detected 2 TimeOps in a row. Ignoring second one: $t")
         else condensed += t
       case i: Implies =>
         condensed += i
@@ -33,16 +33,13 @@ class Sequence[T,H,M](input: SequenceElement*) {
         else if (condensed(i-1).isInstanceOf[Implies]) groupedSeq += new PropSet[T,H,M](a, new TimeOp(cycles = 0, modifier = 0))
         else groupedSeq += new PropSet[T,H,M](a, condensed(i-1).asInstanceOf[TimeOp])
       case t: TimeOp =>
-        if (i == 0) assert(false, s"ERROR: First element of sequence must be a AtmProp. Given TimeOp: $t.")
+        groupedSeq += new PropSet[T,H,M](new AtmProp({(_:T, _: HashMap[String, H], _: Option[SLMemoryState[M]]) => true}, "temp-incomplete propset"), t, incomplete = true)
       case _: Implies =>
         firstImplication = groupedSeq.size
-        if (i == 0) assert(false, s"ERROR: First element of sequence must be a AtmProp. Given Implies.")
         groupedSeq += new PropSet[T,H,M](new AtmProp[T,H,M]({(_:T, _: HashMap[String, H], _: Option[SLMemoryState[M]]) => true}, "Implication"),
           new TimeOp(0), true)
     }
   }
-  // Warnings
-  if (groupedSeq.nonEmpty && firstImplication == -1) println(s"WARNING: Given sequence does not contain an implication.")
   // Temporary warning
   if (input.nonEmpty && input(input.size - 1).isInstanceOf[TimeOp])
     println(s"WARNING: Last element of sequence is TimeOp: ${input(input.size - 1)} and is not matched with an AtmProp.")
@@ -55,7 +52,6 @@ class Sequence[T,H,M](input: SequenceElement*) {
     groupedSeq ++= list
     // Calculating new first Implication
     for ((p, i) <- list.zipWithIndex) if (p.isImplication && firstImplication == -1) firstImplication = i
-    if (firstImplication == -1) println(s"WARNING: Given sequence does not contain an implication.")
   }
   def isEmpty: Boolean = groupedSeq.isEmpty
 
