6c8f2d52a7dab9fcadf2d6ab2160003c8715afec
Ryan Lund <ryan.lund@berkeley.edu>
2021-03-18 11:00:18 -0700

    Add random wait cyclers option to DecoupledDriverSlave monitor

--- core/src/DecoupledVIP.scala
+++ core/src/DecoupledVIP.scala
@@ -93,7 +93,7 @@ class DecoupledDriverMaster[T <: Data](clock: Clock, interface: DecoupledIO[T]) 
 }
 
 // TODO: have this return a stream of seen transactions
-class DecoupledDriverSlave[T <: Data](clock: Clock, interface: DecoupledIO[T], waitCycles: Int = 0) {
+class DecoupledDriverSlave[T <: Data](clock: Clock, interface: DecoupledIO[T], waitCycles: Int = 0, randomWaitCycles: (Int, Int) = (0,0)) {
   assert(DataMirror.directionOf(interface.valid) == Direction.Output, "DecoupledDriverSlave is connected to a slave port, not a master")
   fork.withRegion(TestdriverMain) {
     var cycleCount = 0
@@ -107,7 +107,11 @@ class DecoupledDriverSlave[T <: Data](clock: Clock, interface: DecoupledIO[T], w
       }
       interface.ready.poke(true.B)
       if (interface.valid.peek().litToBoolean) {
-        idleCyclesD = waitCycles
+        if (randomWaitCycles != (0,0)) {
+          idleCyclesD = scala.util.Random.nextInt(randomWaitCycles._2) + randomWaitCycles._1
+        } else {
+          idleCyclesD = waitCycles
+        }
       }
       cycleCount += 1
       clock.step()
